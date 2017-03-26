e3473ff137dea8522e9f813b398ed841fa488c38
dallasconcrete <dallasconcrete24@gmail.com>
2017-03-26 14:07:23 -0500

    removed on('submit') callback

--- scripts/contact.js
+++ scripts/contact.js
@@ -45,42 +45,6 @@ $(document).ready(function() {
         success: function(e) {
             e.text("").addClass("valid");
         }
-    }), $('#contact_form').submit(function() {
-        // submit the form
-        if($(this).valid()){
-            $('#contact_submit').button('loading');
-            var action = $(this).attr('action');
-            $.ajax({
-                url: action,
-                type: 'POST',
-                data: {
-                    contactname: $('#contact_name').val(),
-                    contactaddress: $('#contact_address').val(),
-                    contactreference: $('#contact_reference').val(),
-                    contactphone: $('#contact_phone').val(),
-                    contactemail: $('#contact_email').val(),
-                    contactmessage: $('#contact_message').val(),
-                    contactjob: $('#contact_job').val(),
-                    contacttime: $('#contact_time').val()
-                },
-                success: function() {
-                    $('#contact_submit').button('reset'),
-                    $("#modalContact .modal-title").html('<i class="icon-paper-plane"></i>Thank You for your request!<br>We will be contacting you shortly.'),
-                    $("#modalContact").modal("show"),
-                    $('.tg-login-lightbox').slideToggle(),
-                    $('.modal-backdrop').slideToggle()
-                },
-                error: function() {
-                    $('#contact_submit').button('reset')
-                    $("#modalContact .modal-title").html('<i class="icon-ban"></i>Oh no!<br>Something went wrong!'),
-                    $("#modalContact").modal("show");
-                }
-            });
-        // return false to prevent normal browser submit and page navigation
-        } else {
-            $('#contact_submit').button('reset')
-        }
-        return false;
     });
 
 });
